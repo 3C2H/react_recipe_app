69d413d5bc4567bf73a91bfdfa3bbfcfde49f594
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-01-18 15:00:37 +0100

    add Recipe component

---
+++ src/Components/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Recipe = () => {
+  return (
+    <div>
+      <h1></h1>
+      <p></p>
+      <img src='' alt=''></img>
+    </div>
+  )
+}
+
+export default Recipe
